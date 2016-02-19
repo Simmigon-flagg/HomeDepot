8d31ac2dd07b32f44975b4c0f2b75357ea0100c1
Nathan <asianat2332@yahoo.com>
2016-02-19 18:45:01 -0500

    contact page

---
+++ contact.php
@@ -0,0 +1,24 @@
+<html> 
+
+<head>
+	<title> Contact </title>
+	  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  	  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  	  <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+   	  <link rel="stylesheet" type="text/css" href="contact.css">
+  	  <script type="text/javascript" src="contact.js"></script>
+</head> 
+
+<body>
+
+<div ng-app="" ng-init="name='nathan'">
+ 	<!--	<p>Name : <input type="text" ng-model="name"></p> -->
+
+
+ 	<h1>Contact {{name}}</h1>
+</div>
+
+</body>
+
+</html>
