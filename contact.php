1aa9e418c9869e3a784e0fa3f5aa007a46ef7994
Nathan <asianat2332@yahoo.com>
2016-02-19 20:45:16 -0500

    contact

--- contact.php
+++ contact.php
@@ -12,11 +12,38 @@
 
 <body>
 
+<?php
+$servername = "us-cdbr-azure-east-a.cloudapp.net";
+$username = "bdf7fc6b334bd9";
+$password = "57c924c2";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
+
 <div ng-app="" ng-init="name='nathan'">
  	<!--	<p>Name : <input type="text" ng-model="name"></p> -->
-
-
  	<h1>Contact {{name}}</h1>
+</div>
+
+
+
+<div id="container">
+
+	<div class="section">
+	Phone Number: 
+	</div>
+
+	<br><br>
+	
+	<div class="section">
+	Email: 
+	</div>
+
 </div>
 
 </body>
