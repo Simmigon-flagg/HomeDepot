a469e89cd870ddde557e08de4b582595fc74af2c
Jovy Cortez <jovycortez11@yahoo.com>
2016-02-20 11:18:32 -0500

    Match.php creation

---
+++ data/match.php
@@ -0,0 +1,22 @@
+<?php
+
+// run query
+$query = mysql_query("SELECT * FROM table");
+
+// set array
+$array = array();
+
+// look through query
+while($row = mysql_fetch_assoc($query)){
+
+  // add each row returned into an array
+  $array[] = $row;
+
+  // OR just echo the data:
+  echo $row['username']; // etc
+
+}
+
+// debug:
+print_r($array); // show all array data
+echo $array[0]['username']; // print the first rows username
